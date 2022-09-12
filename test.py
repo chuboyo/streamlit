5d911165d669eabc19e73926c0d8ae91f41bf7c0
chuboyo <akojichubiyojo1997@gmail.com>
2022-09-12 10:58:41 +0100

    ten

--- test.py
+++ test.py
@@ -190,6 +190,6 @@ class VideoTransformer(VideoTransformerBase):
             return img
 
 muted = st.checkbox("Mute")
-webrtc_streamer(key="example", rtc_configuration={ "iceServers": [{"urls": ["stun:stun.services.mozilla.com"]}]},video_transformer_factory=VideoTransformer, video_html_attrs=VideoHTMLAttributes(
+webrtc_streamer(key="example", rtc_configuration={ "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},video_transformer_factory=VideoTransformer, video_html_attrs=VideoHTMLAttributes(
         autoPlay=True, controls=False, muted=muted
     ))
